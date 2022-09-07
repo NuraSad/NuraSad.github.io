2d7722685faf9cc3a3e397d717c24f19852aa189
Ainur Saduova <ainursaduova@maxats-mbp-2.myfiosgateway.com>
2022-09-07 14:25:00 -0400

    React files

---
+++ precache-manifest.52ca9b1319a4d7649bb10144b6f913b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e34bf85e2889610db377",
+    "url": "/static/css/main.3300e4c8.chunk.css"
+  },
+  {
+    "revision": "e34bf85e2889610db377",
+    "url": "/static/js/main.b95e964c.chunk.js"
+  },
+  {
+    "revision": "0fb3cbbe53b9081f41ab",
+    "url": "/static/js/runtime~main.f0cd5190.js"
+  },
+  {
+    "revision": "d3e17586b2b7ced0846d",
+    "url": "/static/css/2.be81b97b.chunk.css"
+  },
+  {
+    "revision": "d3e17586b2b7ced0846d",
+    "url": "/static/js/2.cf92c1d9.chunk.js"
+  },
+  {
+    "revision": "39863e7a3069ce835c13",
+    "url": "/static/js/3.ce46f12c.chunk.js"
+  },
+  {
+    "revision": "760d144815fff6b812726053595b573b",
+    "url": "/static/media/Animal Insting.760d1448.woff"
+  },
+  {
+    "revision": "6f06792dadbc4929a555519e2e3ecb47",
+    "url": "/index.html"
+  }
+];
